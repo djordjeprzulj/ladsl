f3c28ba44aa13b32043be519735242cf4d6dd2e2
MiroslavStefanovic <mstef@uns.ac.rs>
2022-06-29 14:34:43 +0200

    new tests added

--- tests/test_legal_geo.py
+++ tests/test_legal_geo.py
@@ -1,11 +1,9 @@
 import pytest
 from textx import metamodel_for_language
 
-
 @pytest.fixture
 def mm():
     return metamodel_for_language('ladsl')
-
 
 def test_transfer(mm):
 
@@ -14,7 +12,7 @@ def test_transfer(mm):
        from party1
        parcel 23/101
        share 1/1
-       to party1
+       to party2
        share 1/1
     '''
 
@@ -23,9 +21,9 @@ def test_transfer(mm):
     assert len(m.transactions) == 1
     t = m.transactions[0]
     assert t._from[0] == 'party1'
+    assert t.to[0] == 'party2'
     assert (t.from_share[0].nom, t.from_share[0].den) == (1, 1)
     assert (t.to_share[0].nom, t.to_share[0].den) == (1, 1)
-
 
 def test_transfer_multi(mm):
 
@@ -44,16 +42,20 @@ def test_transfer_multi(mm):
     t = m.transactions[0]
     assert len(t._from) == len(t.to) == 2
     assert t._from[0] == 'party11'
+    assert t._from[1] == 'party12'
+    assert t.to[0] == 'party21'
+    assert t.to[1] == 'party22'
     assert (t.from_share[0].nom, t.from_share[0].den) == (1, 2)
+    assert (t.from_share[1].nom, t.from_share[1].den) == (1, 2)
     assert (t.to_share[0].nom, t.to_share[0].den) == (1, 3)
-
+    assert (t.to_share[1].nom, t.to_share[0].den) == (2, 3)
 
 def test_update_party_1(mm):
     model = '''
     update
         party party1
-        firstName "Marko"
-        lastName  "Marković"
+        firstName "John"
+        lastName  "Smith"
     '''
 
     m = mm.model_from_str(model)
@@ -61,15 +63,14 @@ def test_update_party_1(mm):
     assert len(m.transactions) == 1
     t = m.transactions[0]
     assert t.party == 'party1'
-    assert t.first_name == 'Marko'
-    assert t.last_name == 'Marković'
-
+    assert t.first_name == 'John'
+    assert t.last_name == 'Smith'
 
 def test_update_party_2(mm):
     model = '''
     update
         party party1
-        name "Neka firma"
+        name "New Company Name"
     '''
 
     m = mm.model_from_str(model)
@@ -77,14 +78,13 @@ def test_update_party_2(mm):
     assert len(m.transactions) == 1
     t = m.transactions[0]
     assert t.party == 'party1'
-    assert t.name == 'Neka firma'
-
+    assert t.name == 'New Company Name'
 
 def test_update_party_3(mm):
     model = '''
     update
         party party1
-        address "Neka nova adresa"
+        address "New Address"
     '''
 
     m = mm.model_from_str(model)
@@ -92,14 +92,13 @@ def test_update_party_3(mm):
     assert len(m.transactions) == 1
     t = m.transactions[0]
     assert t.party == 'party1'
-    assert t.address == 'Neka nova adresa'
-
+    assert t.address == 'New Address'
 
 def test_update_parcelpart_1(mm):
     model = '''
     update
         parcel 34/101/2
-        landUse liv1
+        landUse vineyard
     '''
 
     m = mm.model_from_str(model)
@@ -109,8 +108,7 @@ def test_update_parcelpart_1(mm):
     assert t.parcel.ko == 34
     assert t.parcel.parcel == 101
     assert t.parcel.part == 2
-    assert t.land_use == 'liv1'
-
+    assert t.land_use == 'vineyard'
 
 def test_update_right(mm):
     model = '''
@@ -131,7 +129,6 @@ def test_update_right(mm):
     assert t.right.ammount == 45000
     assert t.right.percent == 7
 
-
 def test_create_building(mm):
     model = '''
     create
@@ -150,3 +147,38 @@ def test_create_building(mm):
     assert t.party == ['party1', 'party2']
     assert t.share[0].nom == 1
     assert t.share[0].den == 3
+    assert t.share[1].nom == 2
+    assert t.share[1].den == 3
+
+def test_create_site(mm):
+
+    model = '''
+    create development site 34/77
+    geoaggregates 34/17, 34/18
+    '''
+
+    m = mm.model_from_str(model)
+
+    assert len(m.transactions) == 1
+    t = m.transactions[0]
+    assert t.site.ko == 34 and t.site.id == 77
+
+    assert len(t.geoaggregates) == 2
+    g = t.geoaggregates[0]
+    assert g.ko == 34 and g.id == 17
+
+    assert len(t.geoaggregates) == 2
+    g = t.geoaggregates[1]
+    assert g.ko == 34 and g.id == 18
+
+def test_apply_site(mm):
+
+    model = '''
+    apply development site 34/77
+    '''
+
+    m = mm.model_from_str(model)
+
+    assert len(m.transactions) == 1
+    t = m.transactions[0]
+    assert t.site.ko == 34 and t.site.id == 77
